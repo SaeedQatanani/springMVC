8f4108d9df11feee19629ee691649a126661b39a
SaeedQatanani <saeedqatananil@gmail.com>
2022-11-03 16:15:00 +0200

    initial commit

---
+++ mvc/src/main/java/com/saeed/mvc/services/BookService.java
@@ -0,0 +1,44 @@
+package com.saeed.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.saeed.mvc.models.Book;
+import com.saeed.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    public Book updateBook(Long id, String title, String desc,
+    		String lang, Integer numOfPages) {
+    	Book book = bookRepository.findById(id).get();
+    	book.setTitle(title);
+    	book.setDescription(desc);
+    	book.setLanguage(lang);
+    	book.setNumberOfPages(numOfPages);
+        return bookRepository.save(book);
+    }
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);        
+    }
+}
